c848665f02c98567be7eb14acd825ea3e090b878
dmitriy <boss.vede@mail.ru>
2022-09-02 14:13:17 +0700

    second exercise

--- Program.cs
+++ Program.cs
@@ -1,27 +1,45 @@
 ﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 
-Console.WriteLine("Введите число строк");
-int rows = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число столбцов");
-int colums = Convert.ToInt32(Console.ReadLine());
-double [,] matrix = new double[rows, colums];
-bool sign = true;
-for (int i = 0; i < matrix.GetLength(0); i++)
-{
-    Console.Write("[ ");
-    for (int j = 0; j < matrix.GetLength(1); j++)
-    {
-        matrix[i,j] = new Random().NextDouble();
-        matrix[i,j] *= 10;
-        matrix[i,j] = Math.Round(matrix[i,j], 1);
-        Console.Write($"{matrix[i,j]} ");
-    }
-    Console.WriteLine("]");
-}
+// Console.WriteLine("Введите число строк");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число столбцов");
+// int colums = Convert.ToInt32(Console.ReadLine());
+// double [,] matrix = new double[rows, colums];
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+//     Console.Write("[ ");
+//     for (int j = 0; j < matrix.GetLength(1); j++)
+//     {
+//         matrix[i,j] = new Random().NextDouble();
+//         matrix[i,j] *= 10;
+//         matrix[i,j] = Math.Round(matrix[i,j], 1);
+//         Console.Write($"{matrix[i,j]} ");
+//     }
+//     Console.WriteLine("]");
+// }
 
 //Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
 
-
+// Console.WriteLine("Введите число строк");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число столбцов");
+// int colums = Convert.ToInt32(Console.ReadLine());
+// int [,] matrix = new int[rows, colums];
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+//     Console.Write("[ ");
+//     for (int j = 0; j < matrix.GetLength(1); j++)
+//     {
+//         matrix[i,j] = new Random().Next(0,100);
+//         Console.Write($"{matrix[i,j]} ");
+//     }
+//     Console.WriteLine("]");
+// }
+// Console.WriteLine("Введите индексы");
+// int indexRow = Convert.ToInt32(Console.ReadLine());
+// int indexColumn = Convert.ToInt32(Console.ReadLine());
+// if (indexRow >= rows || indexColumn >= colums) System.Console.WriteLine("Такого значения нет");
+// else Console.WriteLine($"Значение с индексом [{indexRow},{indexColumn}] = {matrix[indexRow,indexColumn]}");
 
 //Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 
